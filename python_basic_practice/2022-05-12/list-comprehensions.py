5f82dd725ade5fb1ef1ef12b207fb6481fe5b331
EKnsl <emrul.kais@nextsolutionlab.com>
2022-05-17 09:51:54 +0600

    hackerrank problem list comprehension solved using list comprehension

--- python_basic_practice/2022-05-12/list-comprehensions.py
+++ python_basic_practice/2022-05-12/list-comprehensions.py
@@ -4,12 +4,4 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
-    result = []
-    for i in range(x + 1) :
-        for j in range(y + 1) :
-            for k in range(z + 1) :
-                if i + j + k != n :
-                    result.append([i, j, k])
-    
-    
-    print(result)
+    print([ [i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n])
