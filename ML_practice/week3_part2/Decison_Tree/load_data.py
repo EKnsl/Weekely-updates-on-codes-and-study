56eff270359a27c82d1197e5756ad7716fa239b2
EKnsl <emrul.kais@nextsolutionlab.com>
2022-06-28 09:15:29 +0600

    add decison tree practice notebook in ML_practice week 3 Part 2

--- ML_practice/week3_part2/Decison_Tree/load_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
-pima = pd.read_csv("./data/pima_indians_diabetes.csv", header = None, names = col_names)
-
-print(pima.head())
